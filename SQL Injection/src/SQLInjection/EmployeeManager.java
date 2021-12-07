406efd4afe9be5845c3aa7c2f8494b0a716962a0
ITDSIU19031 <dangmaianhvta2016@gmail.com>
2021-12-07 20:49:42 +0700

    Project

---
+++ SQL Injection/src/SQLInjection/EmployeeManager.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQLInjection;
+
+/**
+ *
+ * @author Nhat Anh
+ */
+public class EmployeeManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginAdmin LA = new LoginAdmin();
+        LA.setVisible(true);
+//        Admin Ad = new Admin();
+//        Ad.setVisible(true);
+    }
+    
+}
